83d1c80b5305b158e7b0b62253e90d5c02ff0d11
RAJAN GAUCHAN <rgrgrg711@gmail.com>
2020-03-21 04:18:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+var dwarves = ["Dopey", "Grumpy", "Bashful"];
 function dwarfRollCall(dwarves) {
+  let listDwarves = [];
+  for(let i = 0; i < dwarves.length; i++){
+    
+    listDwarves.push(`${i+1}. ${dwarves[i]}`);
+    return listDwarves.join("");
+  }
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
+  let uppercasePlaneteerCalls = [];
+  for(let i = 0; i < planeteerCalls.length; i++){
+    let punctuation = "!";
+    uppercasePlaneteerCalls.push(`${planeteerCalls[i]}${punctuation}`.toUpperCase());
+  }
+  return uppercasePlaneteerCalls;
 }
 
 function longPlaneteerCalls(words) {
+  for(let i = 0; i < words.length; i++){
+    
+    if(words[i].length>4){
+      return true;
+    }
+  }
+  return false;
 }
 
 function findTheCheese (foods) {
